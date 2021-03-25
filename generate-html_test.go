e1e9ddb9f9e54789c1dbefadfee9ee4fe45a37ac
fwhez <1728565484@qq.com>
2021-03-25 23:07:38 +0800

    hst

--- generate-html_test.go
+++ generate-html_test.go
@@ -1,27 +1,2 @@
 package model_convert
 
-import (
-	"fmt"
-	"io/ioutil"
-	"testing"
-
-	"github.com/russross/blackfriday"
-)
-
-func TestGenHTML(t *testing.T) {
-
-	buf, e := ioutil.ReadFile(`G:/gitbooks/Library/Import/xyx_games_doc/zhong-zhi-shang-rao-ma-jiang/httpwen-dang-shuo-ming/dui-jie/lian-chu-bei.md`)
-	if e != nil {
-		fmt.Println(e.Error())
-		return
-	}
-
-	output := blackfriday.MarkdownCommon(buf)
-
-	fmt.Println(string(output))
-
-	if e:= ioutil.WriteFile("./tmp/tmp.html", output, 0666); e!=nil {
-		fmt.Println(e.Error())
-		return
-	}
-}
--- model_convert.go
+++ model_convert.go
@@ -7,10 +7,12 @@ import (
 
 	"github.com/atotto/clipboard"
 	"github.com/fwhezfwhez/errorx"
+
+
 	"github.com/jinzhu/gorm"
-	//_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
 )
 
 const (
